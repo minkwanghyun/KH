30195c8e2d9324bc3c966e1ac4008adfd3971421
nolia0637 <bgyg25432@gmail.com>
2026-02-04 01:20:23 +0900

    총알 물리 구현

--- Assets/Undead Survivor/Scriptes/Bullet.cs
+++ Assets/Undead Survivor/Scriptes/Bullet.cs
@@ -5,9 +5,35 @@ public class Bullet : MonoBehaviour
     public float damage;
     public int per;
 
-    public void Init(float damage, int per)
+    Rigidbody2D rigid;
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    public void Init(float damage, int per, Vector3 dir)
     {
         this.damage = damage;
         this.per = per;
+
+        if(per > -1)
+        {
+            rigid.velocity = dir;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)//°üÅë
+    {
+        if (collision.CompareTag("Enemy") || per == -1)
+            return;
+
+        per--;
+
+        if (per == -1)
+        {
+            rigid.velocity = Vector2.zero;
+            gameObject.SetActive(false);
+        }
     }
 }
